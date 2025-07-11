4a4fd304b76d4d72bd6cd25bb95360e1209f744f
GALBICCIM <godvictory0909@gmail.com>
2025-07-11 09:13:53 +0900

    init: initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+# random-chating-project-frontend
+
+랜덤채팅 웹 어플리케이션 프로젝트 프론트엔드
---
+++ src/api/instance.ts
@@ -0,0 +1,5 @@
+import axios, { AxiosInstance } from "axios";
+
+const API_BASEURLS = {
+   AUTH: "https://localhost:3000/auth",
+};
